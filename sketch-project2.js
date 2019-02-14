44c38ef750ca342c549e6f4554660d09951bd132
saf35 <saf35@duke.edu>
2019-02-14 12:49:48 -0500

    first commit

---
+++ sketch-project2.js
@@ -0,0 +1,26 @@
+/*
+Due: Feb 21
+
+Spring color palette: mostly green, with splashes of the rainbow for flowers and blue for the 
+sky
+
+Summer color palette: Blue for sky, sand color for ground
+
+Fall color palette: Dark green/brown for tree trunks, and orange-red & red for leaves and ground,
+maybe with some green
+
+Winter color palette: Very light or very deep blue-purple, with browns and greys, and 
+yellow-orange for the Sun
+*/
+
+let wW = windowWidth;
+let wH = windowHeight;
+
+
+function setup(){
+    createCanvas(wW,wH);
+}
+
+function draw(){
+
+}
