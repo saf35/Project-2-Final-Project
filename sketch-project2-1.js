46402c488640e149425d9c4763ffe02fef1ea755
saf35 <saf35@duke.edu>
2019-02-14 13:01:49 -0500

    diamond rain

--- sketch-project2-1.js
+++ sketch-project2-1.js
@@ -1,10 +1,38 @@
 // Idea: I want objects on the screen to respond to user input, creating a beautiful new planet 
 // environment
 
-function setup(){
+/*
+    Current goal: move diamonds like rain
+*/
 
+function setup(){
+    createCanvas(windowWidth,windowHeight);
 }
 
 function draw(){
+    background(42,28,120);
+}
 
+// I want diamonds, inspired by the planet that has diamond rain
+class Diamond{
+    constructor(tempX,tempY){
+        this.x = tempX;
+        this.y = tempY;
+    }
+
+    // moving diamond rain
+    move(){
+        
+    }
+
+    // diamond shape
+    diaShp(){
+        push();
+        {
+            fill(211,244,237); // light blue color
+            rotate(120); // diamond
+            rect(100,30); // base shape for diamond
+        }
+        pop();
+    }
 }
