a81b449e361c106b457db864edb3d421f36d4b57
saf35 <saf35@duke.edu>
2019-02-19 12:22:54 -0500

    create sea slugs

--- sketch-project2-2.js
+++ sketch-project2-2.js
@@ -1,12 +1,27 @@
 /*
-Idea: sea slugs of different, random colors moving across the screen, leaving slime
+Idea: sea slugs/nudibranchs of different, random colors moving across the screen, leaving slime
 Extra: can give birth???
+Extra: fighting slugs?
 */
 
-function setup(){
+// Current goal: make sea slugs (right now, one type of shape, to keep it simple)
 
+function setup(){
+    createCanvas(windowWidth,windowHeight);
 }
 
 function draw(){
 
 }
+
+class seaSlug{
+    constructor(tempX,tempY){
+        this.x = tempX;
+        this.y = tempY;
+    }
+}
+
+/*
+Goal log (reverse-chronological order):
+- New idea
+*/
