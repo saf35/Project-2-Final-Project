a119ef7b854e2e2b64a130d0bf6592157d361456
saf35 <saf35@duke.edu>
2019-02-20 12:18:12 -0500

    music idea

---
+++ sketch-project2-3.js
@@ -0,0 +1,7 @@
+/* 
+Idea: create ecosystem based on music
+
+- Using flock example: https://p5js.org/examples/simulate-flocking.html
+- Different kinds of animals depend on 
+- Song of choice: Juice by Lizzo
+*/
