804920d2d88d7b032a1ba2ab796de5b856d72ff8
saf35 <saf35@duke.edu>
2019-02-20 12:40:21 -0500

    song choices

--- sketch-project2-3.js
+++ sketch-project2-3.js
@@ -3,5 +3,19 @@ Idea: create ecosystem based on music
 
 - Using flock example: https://p5js.org/examples/simulate-flocking.html
 - Different kinds of animals depend on 
-- Song of choice: Juice by Lizzo
+- Songs of choice:
+    - Falls remix
+    - Face My Fears
+    - Never Ending Circles
+    - ~treading water~
+
+- Not using these songs:
+    - L*** is a Bad Word
+    - BRN
+    - Send My Love to Your New Lover
+    - Don't Think Twice
+    - Keep Running
+    - Down and Out
+    - Now Is Not the Time
+    - Movement
 */
