81549f3fd80daf2a743596b1324948b3bce48fe0
saf35 <saf35@duke.edu>
2019-02-20 19:24:49 -0500

    new idea

--- sketch-project2-3.js
+++ sketch-project2-3.js
@@ -1,21 +1,69 @@
 /* 
-Idea: create ecosystem based on music
+Idea: video game background - something like rhythym games, like Guitar Hero?
 
-- Using flock example: https://p5js.org/examples/simulate-flocking.html
-- Different kinds of animals depend on 
-- Songs of choice:
-    - Falls remix
-    - Face My Fears
-    - Never Ending Circles
-    - ~treading water~
+    - Analyze a photo??
 
-- Not using these songs:
-    - L*** is a Bad Word
-    - BRN
-    - Send My Love to Your New Lover
-    - Don't Think Twice
-    - Keep Running
-    - Down and Out
-    - Now Is Not the Time
-    - Movement
+    - Music choices:
+        - Let's Get Lost
+        - Falls Remix
+
+    - Advice from:
+        - https://p5js.org/examples/sound-measuring-amplitude.html
+
+Other ideas:
+    - Two phones, sending emojis
+    - Falling shiny shapes
+
+Idea: falling shiny shapes
 */
+
+// Current goal: 
+
+let stars = [];
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+    background(36,113,163);
+
+    for(let i = 0; i < stars.length; i++){
+        stars[i].display();
+        stars[i].move();
+    }
+}
+
+class triangles{
+    constructor(tempX,tempY){
+        this.x = tempX;
+        this.y = tempY;
+    }
+
+    move(){
+        let fall = 0.1;
+        if(this.y < windowHeight - 10){
+            this.y += fall;
+        } else {
+            this.y = windowHeight - 10;
+        }
+    }
+
+    display(){
+        push(); {
+            translate(this.x,this.y);
+            if(mouse.x < this.x + 20){ // make triangles glow when the mouse is near
+                color(251,255,162,200);
+            } else if(mouse.x > this.x - 20){
+                color(251,255,162,200);
+            } else if(mouse.y < this.y + 10){
+                color(251,255,162,200);
+            } else if(mouse.y > this.y - 30){
+                color(251,255,162,200);
+            } else{
+                color(208,214,82,100);
+            }
+            triangles(0,-30,20,10,-20,10)
+        } pop();
+    }
+}
